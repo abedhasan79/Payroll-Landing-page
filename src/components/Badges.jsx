70e25e06ae3782b0aeb8f922af3c644d11a90c06
Abed Hasan <abedhasan79@gmail.com>
2025-07-10 17:31:14 -0400

    bagde style change

--- src/components/Badges.jsx
+++ src/components/Badges.jsx
@@ -16,7 +16,7 @@ const Badges = () => {
         {badges.map(({ src, alt }) => (
           <div
             key={alt}
-            className="p-2 rounded-xl shadow-[0_0_15px_3px_rgba(0,191,255,0.2)] transition duration-300  hover:shadow-xl transition"
+            className="p-2 rounded-xl shadow-[0_0_15px_3px_rgba(0,191,255,0.7)] transition duration-300  hover:shadow-xl transition"
           >
             <img
               src={src}
